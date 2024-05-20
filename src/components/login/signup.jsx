be5a7bdad1cd32100c976e0d6d31a83e100bd742
Nakul Gupta <114728605+nakul2207@users.noreply.github.com>
2024-05-20 10:26:11 +0530

    Update signup.jsx

--- src/components/login/signup.jsx
+++ src/components/login/signup.jsx
@@ -4,8 +4,8 @@ function Signup({setloggedIn}){
     return(
         <Form
         title="Welcome To Ecomzy"
-        desc1="Build skills fro today, tomorrow, and beyond."
-        desc2="Education to future-proof your career."
+        desc1="Join us and find everything you need for today, tomorrow, and beyond."
+        desc2="Explore our wide range of clothing and daily necessities to future-proof your style and convenience."
         formtype="signup"
         setloggedIn={setloggedIn}
         ></Form>

76970dea926506ac96688ff301d8d90807298f75
Nakul Gupta <114728605+nakul2207@users.noreply.github.com>
2024-05-20 10:25:04 +0530

    Update login.jsx

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -6,8 +6,8 @@ function Login({setloggedIn,setuser}){
     return(
         <Form
         title="Welcome Back"
-        desc1="Build skills fro today, tomorrow, and beyond."
-        desc2="Education to future-proof your career."
+        desc1="Discover essentials for today, tomorrow, and beyond."
+        desc2="Shop the latest trends and daily necessities to enhance your lifestyle."
         formtype="login"
         setloggedIn={setloggedIn}
         setuser={setuser}
